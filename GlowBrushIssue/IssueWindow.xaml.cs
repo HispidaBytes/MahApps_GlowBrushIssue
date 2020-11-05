f694e626c1126b3892490a5aea797d92fa91d352
HispidaBytes <34475384+HispidaBytes@users.noreply.github.com>
2020-11-05 16:53:50 +0100

    Added files

---
+++ GlowBrushIssue/IssueWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace GlowBrushIssue
+{
+    /// <summary>
+    /// Interaction logic for IssueWindow.xaml
+    /// </summary>
+    public partial class IssueWindow : MetroWindow
+    {
+        public IssueWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GlowBrushIssue/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+
+namespace GlowBrushIssue
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new IssueWindow();
+            window.Show();
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new IssueWindow();
+            window.ResizeMode = ResizeMode.NoResize;
+            window.BorderThickness = new Thickness(0);
+            window.Show();
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new IssueWindow();
+            window.ResizeMode = ResizeMode.NoResize;
+            window.Show();
+        }
+    }
+}
